4004c0d396444174473aa7d29cb3a26400ab408b
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-12-29 14:14:42 +0100

    Simplified tester code

--- TestRegFileParser/Program.cs
+++ TestRegFileParser/Program.cs
@@ -1,98 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 
 using Nefarius.Utilities.Registry;
 
-namespace TestRegFileParser;
+string outputPath = Environment.ExpandEnvironmentVariables("%Temp%\\regfile.txt");
 
-internal class Program
+Console.WriteLine("Registry File Parser");
+
+if (args.Length == 0)
 {
-    private static string _outputPath = Environment.ExpandEnvironmentVariables("%Temp%\\regfile.txt");
+    Console.WriteLine("Missing parameter(s).");
+    Console.WriteLine("Syntax:");
+    Console.WriteLine("RegFileParser <reg file path> [<output file path>]");
+    Console.WriteLine();
 
-    private static void Main(string[] args)
+    //Wait for ENTER key to close program
+    Console.WriteLine("Press 'ENTER' to exit program.");
+    Console.ReadLine();
+
+    return;
+}
+
+if (!File.Exists(args[0]))
+{
+    Console.WriteLine("File '{0}' not found.", args[0]);
+
+    //Wait for ENTER key to close program
+    Console.WriteLine("Press 'ENTER' to exit program.");
+    Console.ReadLine();
+    return;
+}
+
+if (args.Length == 2)
+{
+    outputPath = args[1];
+}
+
+try
+{
+    RegFile regFile = new(args[0]);
+    Console.WriteLine("Reg file has been imported.");
+
+    //For proofing purpose generate new txt file with imported content
+    Int32 count = 0;
+    StringBuilder sb = new();
+    foreach (KeyValuePair<string, Dictionary<string, RegValue>> entry in regFile.RegValues)
     {
-        Console.WriteLine("Registry File Parser");
-
-        if (args.Length == 0)
+        sb.AppendLine($@"[{entry.Key}]");
+        foreach (KeyValuePair<string, RegValue> item in entry.Value)
         {
-            Console.WriteLine("Missing parameter(s).");
-            Console.WriteLine("Syntax:");
-            Console.WriteLine("RegFileParser <reg file path> [<output file path>]");
-            Console.WriteLine();
-
-            //Wait for ENTER key to close program
-            Console.WriteLine("Press 'ENTER' to exit program.");
-            Console.ReadLine();
-
-            return;
-        }
-
-        if (!File.Exists(args[0]))
-        {
-            Console.WriteLine("File '{0}' not found.", args[0]);
-
-            //Wait for ENTER key to close program
-            Console.WriteLine("Press 'ENTER' to exit program.");
-            Console.ReadLine();
-            return;
-        }
-
-        if (args.Length == 2)
-        {
-            _outputPath = args[1];
-        }
-
-        try
-        {
-            RegFile regfile = new(args[0]);
-            Console.WriteLine("Reg file has been imported.");
-
-            //For proofing purpose generate new txt file with imported content
-            Int32 count = 0;
-            StringBuilder sb = new();
-            foreach (KeyValuePair<string, Dictionary<string, RegValue>> entry in regfile.RegValues)
+            if (string.IsNullOrEmpty(item.Value.Entry))
             {
-                sb.AppendLine($@"[{entry.Key}]");
-                foreach (KeyValuePair<string, RegValue> item in entry.Value)
-                {
-                    if (string.IsNullOrEmpty(item.Value.Entry))
-                    {
-                        sb.Append("@=");
-                    }
-                    else
-                    {
-                        sb.AppendFormat("\"{0}\"=", item.Value.Entry);
-                    }
-
-                    if (item.Value.Type == RegValueType.Sz)
-                    {
-                        sb.AppendLine($"\"{item.Value.Value}\"");
-                    }
-                    else
-                    {
-                        sb.AppendLine($"[{item.Value.Type}] {item.Value.Value}");
-                    }
-
-                    count++;
-                }
-
-                sb.AppendLine();
+                sb.Append("@=");
+            }
+            else
+            {
+                sb.Append($"\"{item.Value.Entry}\"=");
             }
 
-            File.WriteAllText(_outputPath, sb.ToString());
-            Console.WriteLine("Content file generated as '{0}'", _outputPath);
+            if (item.Value.Type == RegValueType.Sz)
+            {
+                sb.AppendLine($"\"{item.Value.Value}\"");
+            }
+            else
+            {
+                sb.AppendLine($"[{item.Value.Type}] {item.Value.Value}");
+            }
 
-            Console.WriteLine("Reg file contains {0} keys and {1} values.", regfile.RegValues.Count, count);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Exception thrown\n{0}", ex);
+            count++;
         }
 
-        //Wait for ENTER key to close program
-        Console.WriteLine("Press 'ENTER' to exit program.");
-        Console.ReadLine();
+        sb.AppendLine();
     }
+
+    File.WriteAllText(outputPath, sb.ToString());
+    Console.WriteLine("Content file generated as '{0}'", outputPath);
+
+    Console.WriteLine("Reg file contains {0} keys and {1} values.", regFile.RegValues.Count, count);
 }
+catch (Exception ex)
+{
+    Console.WriteLine("Exception thrown\n{0}", ex);
+}
+
+//Wait for ENTER key to close program
+Console.WriteLine("Press 'ENTER' to exit program.");
+Console.ReadLine();
